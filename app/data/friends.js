9defe5065fb90b62caf6336588c3b934d773fae9
Nikee <rivera.nikee@gmail.com>
2018-08-04 20:23:36 -0400

    added changes to server

---
+++ app/data/friends.js
@@ -0,0 +1,169 @@
+// ========================== Friend Data ========================== //
+
+// This is a variable that holds an array of users.
+// The score is a reference for the user's compatibility with each other.
+var friendArray = [
+    {
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+}
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+},
+{
+    name: "Mimi",
+    photo: "",
+    scores: [
+        1,
+        4,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        2,
+        2
+    ]
+}
+];
+
+// Exports the array for require to reference.
+module.exports = friendArray;
--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-// ================ Dependencies ================ //
+// ========================== Dependencies ========================== //
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// ================ Functions ================ //
+// ========================== Express Configuration ========================== //
+
+// Tells node that we are creating an express server.
+var app = express();
+
+// ========================== Ports========================== //
 
 // Defines a port.
-var PORT = 8080
+var PORT = process.env.PORT || 8080;
 
-function handleRequest(req, res) {
-  
-  // Send the below string to the client when the user visits the POST URL
-  res.end("Connection works!" + req.url);
-}
+// Sets up the express app to handle data parsing.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+// ========================== Routes ========================== //
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// ========================== Listener ========================== //
 
 //Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
-
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function() {
+  // Starts the server.
+  console.log("App listening on PORT" + PORT);
 });
